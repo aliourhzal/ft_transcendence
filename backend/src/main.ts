c1510be988e6699f26cbd567d76f7e56c2a75210
Ayoub Salek <ayoub.salek8599@gmail.com>
2023-06-15 17:10:32 +0100

    fix: relations between frontend(redirect url), backend(prisma migration, postgres port, 42 api client secret)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.use(cookieParser());
 	app.enableCors({
-		origin: 'http://10.11.100.162:3001',
+		origin: 'http://127.0.0.1:3001',
 		credentials: true
 	})
 	await app.listen(3000);
