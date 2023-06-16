9315b787b3ae14b4202fcfeca6cd94df6388f31c
Ayoub Salek <ayoub.salek8599@gmail.com>
2023-06-16 03:41:06 +0100

    update: fill visitor object with server data

--- frontend/app/profile/profileContent.tsx
+++ frontend/app/profile/profileContent.tsx
@@ -9,7 +9,6 @@ import useAxiosFetch from "@/hooks/useAxiosFetch";
 import axios from "axios";
 
 export interface Informations {
-	nickname?: string,
 	wallet?: number,
 	grade?: string,
 	level?: number,
@@ -21,11 +20,24 @@ export interface Informations {
 	wins?: number,
 	losses?: number,
 	password?: boolean,
-	avatar?: File | string | HTMLImageElement,
+	avatar?: string,
 	coverPic?: File | string | HTMLImageElement
 }
 
-export default function ProfileContent() {
+function setValues(visitor:Informations, data:any)
+{
+	visitor.nickName = data.nickname;
+	visitor.fName = data.firstName;
+	visitor.lName = data.lastName;
+	visitor.email = data.email;
+	visitor.wallet = data.wallet;
+	visitor.progress = data.points;
+	visitor.avatar = data.profilePic;
+}
+
+export default function ProfileContent()
+{
+	let visitor: Informations;
 	const [profilePic, setProfilePic] = useState('images/man.png');
 	const [nicknameState, setNickname] = useState('undefined');
 	async function fetchUserData(url: string) {
@@ -33,14 +45,18 @@ export default function ProfileContent() {
 			withCredentials: true
 		})
 		console.log(data);
-		setProfilePic(data.profilePic);
-		setNickname(data.nickname);
+		setValues(visitor, data);//fill visitor object with return server data
 	}
 	useEffect(() => {
 		fetchUserData('http://127.0.0.1:3000/users/profile');
 	}, []);
+
+	setProfilePic(visitor.avatar);
+	setNickname(visitor.nickName);
+
 	return(
-		<section className='w-full flex h-screen'><SideBar nickname={nicknameState} changeNickname={setNickname} pic={profilePic} changePic={setProfilePic} />
+		<section className='w-full flex h-screen'>
+			<SideBar nickname={nicknameState} changeNickname={setNickname} pic={profilePic} changePic={setProfilePic} />
 			<div className="flex flex-col items-center gap-[3vh] w-[100vw] h-[100vh] overflow-y-auto mb-10">
 				<ProfileInfo changePic={setProfilePic} pic={profilePic} nickname={nicknameState} changeNickname={setNickname} />{/* pass argument */}
 				<div className=" playerGameInfo grid grid-cols-1 gap-5 mb-10 w-[90%] h-2/3">
