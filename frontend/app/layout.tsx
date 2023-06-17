30f1b8cb24f388916214611a8006a5017d0c6e0f
aliourhzal <aourhzal@student.1337.ma>
2023-06-17 14:48:47 +0100

    add the favicon

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,20 +1,24 @@
 import './globals.css'
 import { Montserrat } from 'next/font/google'
+import Head from 'next/head'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'pongy',
+	title: 'pongy',
+	icons: {
+		icon: '/images/ping.png'
+	}
 }
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={`${montserrat.className} h-screen w-screen`}>{children}</body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body className={`${montserrat.className} h-screen w-screen`}>{children}</body>
+		</html>
+	)
 }
