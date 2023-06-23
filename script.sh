fa13a3d2e822e9f3d9202e2edc95c4699991cb80
Asalek <ayoub.salek8599@gmail.com>
2023-06-23 02:42:26 +0100

    add: script.sh that run the whole ft_transcendence easily

--- script.sh
+++ script.sh
@@ -9,7 +9,8 @@ fi
 
 (cd ~/Desktop/ft_transcendence/backend && docker-compose up --build -d)
 
-
+(cd ~/Desktop/ft_transcendence/backend && npm i)
+(cd ~/Desktop/ft_transcendence/frontend && npm i)
 (cd backend/src/ && npx prisma migrate dev)
 osascript -e 'tell application "Terminal" to do script "cd ~/Desktop/ft_transcendence/frontend/ && npm run dev"'
 osascript -e 'tell application "Terminal" to do script "cd ~/Desktop/ft_transcendence/backend/src && npx prisma studio"'
