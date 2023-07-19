05be4af101f5ba2d456a75c1b58ebcc1ba26bae5
Asalek <ayoub.salek8599@gmail.com>
2023-07-19 21:38:30 +0100

    update: testing webSocket

--- frontend/app/[nickname]/game/page.tsx
+++ frontend/app/[nickname]/game/page.tsx
@@ -1,13 +1,34 @@
 import Script from "next/script";
-import Navbar from "../../login/navbar";
-// import SideBar from "../profile/sideBar";
+import Navbar from "../login/navbar";
+import SideBar from "../profile/sideBar";
+import { WebsocketContext } from "../context_sockets/gameWebSocket";
+import { useContext, useEffect } from "react";
+
 
 export default function Game()
 {
+    const socket = useContext(WebsocketContext);
+    socket.on('connect', () => {
+        console.log('connected');
+        // socket.emit('newMessageAsalek', 'i\'m connected');
+    });
+    useEffect(() => {
+            startGame();
+            socket.on('onMessage', (data) => {
+                console.log(data);
+            });
+            return () => {
+                socket.off('connect');
+                socket.off('onMessage');
+                console.log("disconnect");
+            }
+        }
+    , []);//each time player Y or Ball X | Y change trigger this effect
+
     let width:string = '800px';
     return (
         <section className="flex w-full h-full items-center bg-darken-200">
-            {/* <SideBar/ > */}
+            <SideBar/ >
             <div className="flex flex-col items-center w-full gap-5">
                 <div className="w-full flex justify-center gap-96">
                     <div className="flex items-center gap-x-5">
@@ -22,8 +43,7 @@ export default function Game()
                 <canvas id="pongy" className="bg-darken-300 mx-auto rounded-3xl w-[{width}] h-[450x]" width={width} height="450px"></canvas>
             </div>
             {/* <Navbar/ > */}
-            <Script src="../../game-script.js" defer></Script>
+            {/* <Script src="../../game-script.js" defer></Script> */}
         </section>
     );
 }
-
