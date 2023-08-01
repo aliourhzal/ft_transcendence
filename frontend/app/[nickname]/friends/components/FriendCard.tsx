ea8269617a934d7afd895ad28da02cf37648f04a
aliourhzal <aourhzal@student.1337.ma>
2023-08-01 15:44:50 +0100

    solve the problem with the dynamic friend cover picture

--- frontend/app/[nickname]/friends/components/FriendCard.tsx
+++ frontend/app/[nickname]/friends/components/FriendCard.tsx
@@ -5,17 +5,14 @@ import { UniversalData } from "../../layout";
 export default function FriendCard({user}: {
 	user: UniversalData
 }) {
-	// const coverStyle = `bg-[url(${user.coverPic})] bg-cover bg-center bg-no-repeat w-[100%] h-[150px]`;
-	// console.log(user.coverPic);
 	return (
-		<h1>hello</h1>
-		// <Container className="p-0 overflow-hidden flex flex-col items-center relative">
-		// 	<div className={coverStyle}></div>
-		// 	<div className="translate-y-[-50%] flex flex-col items-center">
-		// 		<img src={user.profilePic} alt="avatar" className="h-[130px] w-[130px] rounded-full" />
-		// 		<span className="text-white font-medium text-lg">{user.nickname}</span>
-		// 	</div>
-		// 	<div className="absolute top-0 left-0 h-full w-full z-10 bg-black opacity-50"></div>
-		// </Container>
+		<Container className="p-0 overflow-hidden flex flex-col items-center relative">
+			<div style={{backgroundImage: `url(${user.coverPic})`}} className="bg-cover bg-center bg-no-repeat w-[100%] h-[150px] rounded-lg"></div>
+			<div className="translate-y-[-50%] flex flex-col items-center">
+				<img src={user.profilePic} alt="avatar" className="h-[130px] w-[130px] rounded-full" />
+				<span className="text-white font-medium text-lg">{user.nickname}</span>
+			</div>
+			{/* <div className="absolute top-0 left-0 h-full w-full z-10 bg-black opacity-50"></div> */}
+		</Container>
 	);
 }
