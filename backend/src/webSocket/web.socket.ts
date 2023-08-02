b58af0f7685913626c8819f71eaa3c778464653a
aliourhzal <aourhzal@student.1337.ma>
2023-08-02 16:16:04 +0100

    create basic game logic in backend

--- backend/src/webSocket/web.socket.ts
+++ backend/src/webSocket/web.socket.ts
@@ -1,29 +1,124 @@
-import { Body, OnModuleInit } from "@nestjs/common";
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
-import { Server } from 'socket.io'
+import { UnauthorizedException } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { randomUUID } from "crypto";
+import { Server, Socket } from 'socket.io'
+import { UsersService } from "src/users/users.service";
+
+type userNode = {
+	socket: Socket,
+	nickname: string
+}
+
+class Ball {
+	x: number;
+	y: number;
+
+	top: number;
+	bottom: number;
+	left: number;
+	right: number;
+
+	radius = 10;
+	velocityX = 5; //ball direction
+	velocityY = 5;
+	speed = 7;
+	color = "WHITE";
+
+	constructor(initX: number, initY: number) {
+		this.x = initX;
+		this.y = initY;
+	}
+}
 
 @WebSocketGateway(3003, { cors: true } ) //tell's the class that it using socket not http and use the port 3003 instead of default one 3000
-export class myGateAway implements OnModuleInit
+export class myGateAway implements OnGatewayConnection, OnGatewayDisconnect
 {
-    @WebSocketServer()
-    server:Server;
+	@WebSocketServer()
+	server:Server;
 
-    onModuleInit()//directly after a client connect to the socket do :
+	private connectedUsers: userNode[] = [];
+
+	constructor(
+		private readonly jwtService: JwtService,
+		private readonly usersService: UsersService	
+	) {}
+
+	OnWebSocektError(socket:Socket)
     {
-        //connect or connection both works
-        this.server.on('connect', (socket) => {
-            console.log("socker ID: " + socket.id);
-            console.log('connected successfuly to webSocket');
-        });
+        socket.emit("error", new UnauthorizedException());
+        socket.disconnect();
     }
 
-    @SubscribeMessage('gameData') //get events in websocket protocol
-    onNewMessage(@MessageBody() messageContent: any)// once the subscribeMessage event triggerd log the recieved message
-    {
-        console.log(messageContent);
-        this.server.emit('onMessage', { //when ever a newMessageAsalek event triggers send back an event 'onMessage' object
-            msg: 'new message resend',
-            content: messageContent,
-        });
-    }
+	handleDisconnect(socket: Socket) {
+		console.log('the user disconnected from game socket')
+	}
+
+	update(ball: Ball, roomId: string) {
+		// if (ball.x - ball.radius < -20 || ball.x + ball.radius > 820)
+		// {
+		// 	ball.speed = 7;
+		// 	ball.x = 400;
+		// 	ball.y = 225;
+		// 	ball.velocityY = 5;
+		// 	ball.velocityX = 5;
+		// }
+		ball.x += ball.velocityX;
+		ball.y += ball.velocityY;
+		if(ball.y - ball.radius < 0 || ball.y + ball.radius > 450){
+			ball.velocityY = -ball.velocityY;
+		}
+		if (ball.x - ball.radius < 0 || ball.x - ball.radius >= 800)
+			ball.velocityX = -ball.velocityX;
+		this.server.to(roomId).emit('ball-position', {
+			x: ball.x,
+			y: ball.y
+		})
+	}
+
+	async startGame(roomId: string) {
+		const ball = new Ball(400, 225);
+		let loop:NodeJS.Timer = null;
+		// function game()
+		// {
+		// 	update(ball, roomId);
+		// }
+		let framePerSecond = 50;
+		loop = setInterval(() => {
+			this.update(ball, roomId);
+		},1000/framePerSecond);
+		return ;
+	}
+
+	async createGameRoom() {
+		const roomId = randomUUID();
+		console.log(roomId);
+		this.connectedUsers[0].socket.join(roomId);
+		this.connectedUsers[1].socket.join(roomId);
+		this.connectedUsers.splice(0, 2);
+		this.startGame(roomId);
+	}
+
+	async handleConnection(socket: Socket) {
+		console.log('the user connected to game socket');
+		let decodeJWt: any;
+		try {
+			decodeJWt = this.jwtService.verify(socket.handshake.auth.token, {
+				secret: process.env.JWT_SECRET
+			})
+		} catch (err) {
+			this.OnWebSocektError(socket);
+		}
+		if (!decodeJWt)
+			this.OnWebSocektError(socket);
+		const user = await this.usersService.findOneByNickname(decodeJWt.nickname);
+		if (!user) {
+			this.OnWebSocektError(socket);
+		}
+		this.connectedUsers.push({socket, nickname: user.nickname});
+		if (this.connectedUsers.length < 2)
+			return ;
+		this.createGameRoom();
+	}
+
 }
