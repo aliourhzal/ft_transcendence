e0a32cc7ab51055be687712711e6b7fb4696273f
aliourhzal <aourhzal@student.1337.ma>
2023-08-02 16:14:52 +0100

    update: import jwtModule and usersModule

--- backend/src/webSocket/web.module.ts
+++ backend/src/webSocket/web.module.ts
@@ -1,8 +1,15 @@
 import { Module } from "@nestjs/common";
 import { myGateAway } from "./web.socket";
+import { JwtModule } from "@nestjs/jwt";
+import { UsersModule } from "src/users/users.module";
+import { UsersService } from "src/users/users.service";
 // import { SocketGateway } from "./web.socket";
 
 @Module({
-    providers: [myGateAway],
+    imports: [
+        JwtModule,
+        UsersModule
+    ],
+    providers: [myGateAway, UsersService],
 })
 export class gateAwayModule {}
