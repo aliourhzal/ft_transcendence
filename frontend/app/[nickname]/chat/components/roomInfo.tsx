9b75e641f6f0b556e68702db5125008c901f2fbf
etahae <tahanamir@hotmail.com>
2023-08-06 15:57:23 +0100

    roomInfoForm ready to be linked with backend

--- frontend/app/[nickname]/chat/components/roomInfo.tsx
+++ frontend/app/[nickname]/chat/components/roomInfo.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { gimmeRandom } from '../page'
 import Popup from './Popup'
-import { ImVolumeMute2 } from "react-icons/Im";
+
+import { BiVolumeMute } from "react-icons/Bi";
+import { BiUserMinus } from "react-icons/Bi";
+import { BiUserX } from "react-icons/Bi";
+import { TbUserUp } from "react-icons/Tb";
+import { TbUserDown } from "react-icons/Tb";
+import { BiMessageRoundedDetail } from "react-icons/Bi";
+
 import { UniversalData } from '../../layout';
-import { Avatar } from '@nextui-org/react';
+import { Avatar, useSSR } from '@nextui-org/react';
 
 interface RoomInfoProps {
     room:any
@@ -12,15 +19,38 @@ interface RoomInfoProps {
     userData: UniversalData
 }
 
-// const isAdmin = () => {
-//     return 
-// }
 
 const RoomInfo: React.FC<RoomInfoProps> = (info) => {
-
+    
     const hide = () => {
         info.setShow(false)
     }
+    
+    const isAdmin = (user) => {
+        if (user.type === 'OWNER' || user.type === 'ADMIN')
+            return true
+        return false
+    }
+
+    const promoteUser = () => {
+
+    }
+    const demoteteUser = () => {
+
+    }
+    const kickUser = () => {
+
+    }
+    const banUser = () => {
+        
+    }
+    const muteUser = () => {
+
+    }
+
+    console.log(info.room.users.find(o => o.nickName === info.userData.nickname).type)
+
+    // const [cursor, setCursor] = useState(isAdmin(info.room.users.find(o => o.nickName === info.userData.nickname)) ? 'pointer' : 'not-allowed')
 
   return (
     <Popup isOpen={info.show} modalAppearance={hide}>
@@ -32,13 +62,24 @@ const RoomInfo: React.FC<RoomInfoProps> = (info) => {
             {info.room.users.map(user => (
                     <div className='m-2 border-2 p-2 rounded-lg bg-slate-600 border-slate-500 w-full flex flex-col items-center justify-center' key={user.id}>
                         <div className='m-1 w-full flex justify-between items-center'>
-                            <div className='font-bold'>{user.nickName}</div>
+
+                            <div className='font-bold flex justify-between items-center'>
+                                <Avatar src={info.room.users.find(o => o.nickName === user.nickName).photo}/>
+                                <div className='ml-3'>{user.nickName === info.userData.nickname ? 'you' : user.nickName}</div>
+                            </div>
                             <div className='w-[40%] flex justify-between'>
-                                <img className='cursor-pointer' alt='ban' title='ban' src='/images/ban-user-icon.svg' width={25} height={25}></img>
-                                <img className='cursor-pointer' alt='kick' title='kick' src='/images/kick-user-icon.svg' width={30} height={30}></img>
-                                <ImVolumeMute2 className='cursor-pointer' size={30}/>
-                                {/* <img className='cursor-pointer' alt='mute' title='mute' src='/images/mute-user-icon.svg' width={30} height={30}></img> */}
-                                <img className='cursor-pointer' alt='promote' title='promote' src='/images/promote-user-icon.svg' width={30} height={30}></img>
+                                {(isAdmin(info.room.users.find(o => o.nickName === info.userData.nickname)) && user.nickName != info.userData.nickname) &&
+                                    <>
+                                        { isAdmin(info.room.users.find(o => o.nickName === user.nickName)) ? 
+                                            <TbUserDown title='demote' aria-label='demote' cursor="pointer" size={25} onClick={demoteteUser}/>
+                                            :   <TbUserUp title='promote' aria-label='promote' cursor="pointer" size={25} onClick={promoteUser}/>
+                                        }
+                                        <BiUserMinus title='kick' aria-label='kick' cursor="pointer" size={25} onClick={kickUser}/>
+                                        <BiUserX title='ban' aria-label='ban' cursor="pointer" size={25} onClick={banUser}/>
+                                        <BiVolumeMute title='mute' aria-label='mute' cursor="pointer" size={25} onClick={muteUser}/>
+                                    </>}
+                                { user.nickName != info.userData.nickname &&
+                                <BiMessageRoundedDetail title='DM' aria-label='DM' cursor="pointer" size={25}/>}
                             </div>
                         </div>
                     </div>
