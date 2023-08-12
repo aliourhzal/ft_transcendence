e217ceebb7de4b3fe3dc5603c67d719821e436f0
aliourhzal <aourhzal@student.1337.ma>
2023-08-12 13:37:50 +0100

    add sendRequest when consulting other users profile

--- frontend/app/[nickname]/components/ProfileInfo.tsx
+++ frontend/app/[nickname]/components/ProfileInfo.tsx
@@ -1,18 +1,45 @@
 'use client'
 
-import { useState } from "react";
-import ChangePicIcon from "./ChangePicIcon";
+import { useContext, useEffect, useState } from "react";
 
-import { UniversalData, userDataContext } from "../layout";
+import { UniversalData } from "../layout";
+import { InvitationSocketContext } from "@/app/context_sockets/InvitationWebSocket";
 import axios from "axios";
 
 
-export default function ProfileInfo(props: {data: UniversalData})
+export default function ProfileInfo(props: {data: UniversalData, currentUser: boolean})
 {
 	const levelProgress = `${props.data.level.toString().split('.')[1]}%`;
+	const socket = useContext(InvitationSocketContext);
+	const [showSendRequest, setShowSendRequest] = useState(!props.currentUser);
+
+	async function sendFriendRequest() {
+		socket.emit('send-request', {
+			friend: props.data.nickname
+		});
+	}
+
+	useEffect(() => {
+		if (!props.currentUser) {
+			axios.get('http://127.0.0.1:3000/users/friend/checkFriend', {
+				params: {
+					nickname: props.data.nickname
+				},
+				withCredentials: true
+			})
+			.then(res => {
+				console.log(res.data);
+				setShowSendRequest(res.data);
+			})
+		}
+		socket.on('receive-friends', data => {
+			if (data.nickname === props.data.nickname)
+				setShowSendRequest(false);
+		});
+	}, [])
 
 	return (
-		<div style={{backgroundImage: `url(${props.data.coverPic})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center'}} className="text-md flex flex-col justify-between items-center md:justify-end md:items-end gap-3 w-[90%] mt-[3%] min-h-[550px] h-1/3 rounded-3xl relative">
+		<div style={{backgroundImage: `url(${props.data.coverPic})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center'}} className="text-md flex flex-col justify-between items-center md:justify-end md:items-end gap-3 w-[90%] mt-[3%] min-h-[700px] md:min-h-[550px] h-1/3 rounded-3xl relative">	
 			<div className="flex justify-center md:justify-start w-[100%] md:w-[70%] mt-4 md:mt-0">
 				<div className="flex w-[90%] md:w-[70%] bg-darken-100/70 backdrop-blur-md justify-around py-3 rounded-full">
 					<div className="flex gap-0 flex-col sm:flex-row items-center md:gap-2">
@@ -29,13 +56,13 @@ export default function ProfileInfo(props: {data: UniversalData})
 					</div>
 				</div>
 			</div>
-			<div className="flex flex-col md:flex-row items-center w-[100%] bg-darken-100/70 backdrop-blur-md h-[40%] rounded-b-3xl">{/* the fname lname div */}
+			<div className="flex flex-col md:flex-row items-center w-[100%] bg-darken-100/70 backdrop-blur-md h-[50%] md:h-[40%] rounded-b-3xl">{/* the fname lname div */}
 				<div className="w-full md:w-[30%] h-[90px] md:h-full">
 					<div style={{backgroundImage: `url(${props.data.profilePic as string})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundSize: 'cover'}} className=' flex items-end justify-center p-3 translate-y-[-50%] max-w-[150px] md:max-w-[250px] min-w-[150px] md:w-[70%] w-[40%] h-[auto] aspect-square m-[auto] rounded-full'>
 					</div>
 				</div>
 				<div className="flex flex-col justify-evenly items-center md:items-start w-[100%] md:w-[70%] h-full">
-					<div className="flex justify-evenly md:gap-20 md:justify-start w-[90%] ">{/* row 1 */}
+					<div className="flex flex-wrap justify-evenly md:justify-between gap-3 w-[90%] ">{/* row 1 */}
 						<div className="flex flex-col gap-1 items-start">
 							<h2 className="text-gray-500 text-sm">First Name</h2>
 							<p className="text-whiteSmoke">{props.data.firstName}</p>
@@ -45,7 +72,7 @@ export default function ProfileInfo(props: {data: UniversalData})
 							<p className="text-whiteSmoke">{props.data.lastName}</p>
 						</div>
 						<div className="flex flex-col gap-1 items-start ">
-							<h2 className="text-gray-500 text-sm">Nick Name</h2>
+							<h2 className="text-gray-500 text-sm">Nickname</h2>
 							<p className="text-whiteSmoke">{props.data.nickname}</p>
 						</div>
 						<div className="flex flex-col items-center gap-1 md:items-start ">
@@ -56,6 +83,12 @@ export default function ProfileInfo(props: {data: UniversalData})
 					<div className=" w-[90%] rounded-full bg-darken-300 h-9">
 						<div className="flex items-center justify-end bg-blue-600 text-xs h-full font-medium text-blue-100 text-center p-2 leading-none rounded-full" style={{width: levelProgress}}>{levelProgress}</div>
 					</div>
+					{
+						!props.currentUser && showSendRequest && 
+						<div className="w-[90%] flex justify-center">
+							<button className="py-2 px-4 bg-blueStrong text-white rounded-xl font-medium" onClick={sendFriendRequest}>send Request</button>
+						</div>
+					}
 				</div>
 			</div>
 		</div>
