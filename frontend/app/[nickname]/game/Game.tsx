7106c7cbe1966aa0e0363506defa7f9454e6ba44
aliourhzal <aourhzal@student.1337.ma>
2023-08-13 15:17:07 +0100

    display opponent info

--- frontend/app/[nickname]/game/Game.tsx
+++ frontend/app/[nickname]/game/Game.tsx
@@ -17,19 +17,14 @@ export default function Game(props: any)
 {		
 	const socket = useContext(WebsocketContext);
 	const userData = useContext(userDataContext);
-	const [nickname, setN] = useState("");
-	const [avatar, setA] = useState("");
-	const [com, setCom] = useState(false);
-	let opData : {nickname: string, profilePic: string} = {
-		nickname: "notSetYet",
-		profilePic: ""
-	}
+	const [opData, setOpData] = useState<{nickname: string, avatar: string}>({
+		nickname: '',
+		avatar: ''
+	});
 
 	useEffect(()=>{
-
-		socket.on("playersInfo", (data: {nickname:string, profilePic:string}) => {
-			opData = {...data};
-			setCom(true);
+		socket.on("playersInfo", (data: {nickname:string, avatar:string}) => {
+			setOpData({...data});
 		});
 	}
 	,[]);
@@ -37,16 +32,20 @@ export default function Game(props: any)
 	return (
 		<section className="flex w-full h-full items-center bg-darken-200">
 			{
-				com &&
 				<div className="flex flex-col justify-center items-center w-full gap-5 h-full">
 					<div className="w-[80%] aspec flex justify-between max-sm:">
 						<div className="flex items-center gap-x-5">
-							<img className="w-16 h-16" src={userData.profilePic} alt="man_hhhh" />
+							<img className="w-16 h-16 rounded-full" src={userData.profilePic} alt="man_hhhh" />
 							<h2 className=" text-whiteSmoke">{userData.nickname}</h2>
 						</div>
 						<div className="flex items-center gap-x-5">
-							<h2 className="text-whiteSmoke">{opData.nickname}</h2>
-							<img className="w-16 h-16" src={opData.profilePic} alt="man_hhhh" />
+							{
+								opData.nickname !== '' &&  
+								<>
+									<h2 className="text-whiteSmoke">{opData.nickname}</h2>
+									<img className="w-16 h-16 rounded-full" src={opData.avatar} alt="man_hhhh" />
+								</>
+							}
 						</div>
 					</div>
 					<Canvas socket={socket} themeN={props.themeN} ball={props.ball}  hell={props.hell} />
