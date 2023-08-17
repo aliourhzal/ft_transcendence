4ff8c8b3637a58cdc95aa01da519ab46f08a02c5
etahae <tahanamir@hotmail.com>
2023-08-17 08:59:00 +0100

    working on better notifications

--- frontend/app/[nickname]/chat/components/Notification.tsx
+++ frontend/app/[nickname]/chat/components/Notification.tsx
@@ -1,17 +1,38 @@
 import React, { useState } from 'react'
+import { gimmeRandom } from '../page'
+import {ImCheckmark} from 'react-icons/Im'
+import { GiCrossMark } from 'react-icons/Gi'
 
 interface NotificationProps {
-  text: string
+  notifications: string[]
   type: string
+  notify: boolean
+  setNotify: any
 }
 
-const Notification:React.FC<NotificationProps> = ( { text, type } ) => {
+const Notification:React.FC<NotificationProps> = ( { notifications, type, notify, setNotify } ) => {
 
-  const [bgColor, setBgColor] = useState(type === 'good' ? 'bg-green-600' : 'bg-red-600')
+  // const [bgColor, setBgColor] = useState(type === 'good' ? 'bg-green-600' : 'bg-red-600')
+
 
   return (
-    <div className='z-50 transition ease-in duration-300 absolute top-5 right-5 w-full flex items-center justify-end'>
-        <div className={'rounded-xl p-5 flex items-center h-10 ' + bgColor + ' text-white'}>{text}</div>
+    <div>
+      <button id="notification_button" onClick={() => { setNotify(old => !old) }}>
+				<svg viewBox="0 0 448 512" id="bell"><path d="M224 0c-17.7 0-32 14.3-32 32V49.9C119.5 61.4 64 124.2 64 200v33.4c0 45.4-15.5 89.5-43.8 124.9L5.3 377c-5.8 7.2-6.9 17.1-2.9 25.4S14.8 416 24 416H424c9.2 0 17.6-5.3 21.6-13.6s2.9-18.2-2.9-25.4l-14.9-18.6C399.5 322.9 384 278.8 384 233.4V200c0-75.8-55.5-138.6-128-150.1V32c0-17.7-14.3-32-32-32zm0 96h8c57.4 0 104 46.6 104 104v33.4c0 47.9 13.9 94.6 39.7 134.6H72.3C98.1 328 112 281.3 112 233.4V200c0-57.4 46.6-104 104-104h8zm64 352H224 160c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7s18.7-28.3 18.7-45.3z"></path></svg>
+			</button>
+      {notify && <div className='rounded-xl gap-1 absolute z-50 right-[4%] top-[6%] flex flex-col items-center justify-center' onBlur={ () => {
+          setNotify(false)
+        }}>
+        {
+          notifications.map(notif =>
+             <ul className={'bg-darken-300 hover:bg-slate-600 border-2 rounded-xl p-5 flex items-center h-10 text-white'} key={gimmeRandom()}>
+              {type === 'good' ? <ImCheckmark color='green' className='m-2'/> : <GiCrossMark color='red' className='m-2'/>}
+              {notif}
+            </ul>
+          )
+        }
+      </div>
+      }    
     </div>
   )
 }
--- frontend/app/[nickname]/chat/page.tsx
+++ frontend/app/[nickname]/chat/page.tsx
@@ -128,16 +128,20 @@ export default function Chat() {
 	const [convs, setConvs] = useState<conversation[]>([])
 
 	const [notify, setNotify] = useState(false)
-	const [notif_text, set_notif_text] = useState('')
+	const [notifications, setNotifications] = useState<string[]>([])
 	const [notif_type, set_notif_type] = useState('')
 	const _notification = (_notif_text: string, _notif_type: string) => {
-		set_notif_text(_notif_text)
-		set_notif_type(_notif_type)
+		// set_notif_text(_notif_text)
+		setNotifications(_param => {
+			_param.unshift(_notif_text)
+			return _param
+		})
+		// set_notif_type(_notif_type)
 		setNotify(true)
-		setInterval(() => {
-			setNotify(false)
-			return clearInterval
-		}, 5000)
+		// setInterval(() => {
+		// 	setNotify(false)
+		// 	return clearInterval
+		// }, 5000)
 	}
 
 	const scrollToBottom = () => {
@@ -147,13 +151,10 @@ export default function Chat() {
 
 	return (
 		<main className='select-none h-full w-full'>
-			<button id="notification_button" onClick={() => { setNotify(old => !old) }}>
-				<svg viewBox="0 0 448 512" id="bell"><path d="M224 0c-17.7 0-32 14.3-32 32V49.9C119.5 61.4 64 124.2 64 200v33.4c0 45.4-15.5 89.5-43.8 124.9L5.3 377c-5.8 7.2-6.9 17.1-2.9 25.4S14.8 416 24 416H424c9.2 0 17.6-5.3 21.6-13.6s2.9-18.2-2.9-25.4l-14.9-18.6C399.5 322.9 384 278.8 384 233.4V200c0-75.8-55.5-138.6-128-150.1V32c0-17.7-14.3-32-32-32zm0 96h8c57.4 0 104 46.6 104 104v33.4c0 47.9 13.9 94.6 39.7 134.6H72.3C98.1 328 112 281.3 112 233.4V200c0-57.4 46.6-104 104-104h8zm64 352H224 160c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7s18.7-28.3 18.7-45.3z"></path></svg>
-			</button>
+			<Notification notifications={notifications} type={notif_type} notify={notify} setNotify={setNotify}/>
 			<Context.Provider value={{alertNewMessage, setAlertNewMessage, ref, showConv, setShowConv, activeUserConv, setActiveUserConv, socket,
 				showForm, setShowForm, setChatBoxMessages, chatBoxMessages, userData, showJoinForm, setShowJoinForm, msg_sent, set_msg_sent,
 				set_room_created, room_created, rooms, setRooms, showSearchUsersForm, setShowSearchUsersForm, scrollToBottom, _notification, convs,setConvs}}>
-				{notify && <Notification text={notif_text} type={notif_type}/>}
 				<div id='main' className="flex items-center gap-[3vh] flex-grow h-full overflow-y-auto bg-darken-200">
 					<div className="flex flex-col items-center justify-center w-[100%] text-sm lg:text-base md:relative md:w-[calc(90%/2)] h-[90vh] text-center">
 						<ConvList />
