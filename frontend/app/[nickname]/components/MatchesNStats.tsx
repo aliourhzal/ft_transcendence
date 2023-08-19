2799986e96b079bccfcaeeff71ec70319f270bd6
Ayoub Salek <asalek@e3r1p4.1337.ma>
2023-08-19 19:44:00 +0100

    update: show user history in his profile section

--- frontend/app/[nickname]/components/MatchesNStats.tsx
+++ frontend/app/[nickname]/components/MatchesNStats.tsx
@@ -5,62 +5,105 @@ import { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import { userDataContext } from '../layout';
 
-interface StatsProps {
-    for: string,
-    total: number,
-    wins: number,
-    losses: number
+
+interface matchTemplate {
+    player1: {
+        avatar: string,
+        nickname: string,
+        score: number
+    },
+    player2: {
+        avatar: string,
+        nickname: string,
+        score: number
+    }
 }
 
-function MatchResult() {
+function MatchResult(props) {
     return (
         <div className='w-full rounded-xl flex items-center justify-between bg-darken-300 p-3 gap-2'>
             <div className='flex gap-2 items-center'>
-                <img className='w-10 h-10 rounded-full' src="images/42.jpg" alt="avatar1"/>
-                <span className='text-white font-medium hidden avatarNickname'>Nickname</span>
+                <img className='w-10 h-10 rounded-full' src={props.av1} alt="avatar1"/>
+                <span className='text-white font-medium hidden avatarNickname'>{props.p1}</span>
             </div>
             <div className='text-white flex gap-1 '>
-                <span>15</span>
+                <span>{props.score1}</span>
                 <span>:</span>
-                <span>12</span>
+                <span>{props.score2}</span>
             </div>
             <div className='flex gap-2 items-center'>
-                <span className='text-white font-medium hidden avatarNickname'>Nickname</span>
-                <img className='w-10 h-10 rounded-full' src="images/42.jpg" alt="avatar1"/>
+                <span className='text-white font-medium hidden avatarNickname'>{props.p2}</span>
+                <img className='w-10 h-10 rounded-full' src={props.av2} alt="avatar1"/>
             </div>
         </div>
     );
 }
 
 export function MatchHistory() {
+    const [historyState, setHistory] = useState<matchTemplate[]>([]);
+    const his : matchTemplate[] = [];
+    const matchTemplate : matchTemplate = {
+        player1: {
+            avatar: "",
+            nickname: "",
+            score: 0
+        },
+        player2: {
+            avatar: "",
+            nickname: "",
+            score: 0
+        }
+    };
+    useEffect(()=>{
+        axios.get('http://127.0.0.1:3000/users/profile/history',{
+            withCredentials: true
+        })
+        .then(res => {
+            res.data.forEach(x => {
+                const match: matchTemplate = { ...matchTemplate };
+                match.player1.avatar = x.player1.avatar;
+                match.player1.nickname = x.player1.nickname;
+                match.player1.score = x.player1.score;
+                match.player2.avatar = x.player2.avatar;
+                match.player2.nickname = x.player2.nickname;
+                match.player2.score = x.player2.score;
+                his.push(match);
+            });
+            setHistory(his);
+        })
+        .catch(e => {
+            console.log(e);
+        })
+    }, []);
+
     return(
         <Container className='p-5 bg-darken-100 rounded-xl flex flex-col gap-5 h-full'>
             <h2 className='text-white'>Matches History</h2>
             <div className='matchHistoryBody flex flex-col gap-3 overflow-y-auto max-h-[420px]'>
-                <MatchResult />
-                <MatchResult />
-                <MatchResult />
-                <MatchResult />
-                <MatchResult />
-                <MatchResult />
-                <MatchResult />
-                <MatchResult />
-                <MatchResult />
-                <MatchResult />
-                <MatchResult />
-                <MatchResult />
-                <MatchResult />
-                <MatchResult />
-                <MatchResult />
+                {
+                    historyState.map(x => {
+                        let i = 0;
+                        return (<MatchResult
+                            key={i++}
+                            p1={x.player1.nickname}
+                            av1={x.player1.avatar}
+                            score1={x.player1.score}
+                            p2={x.player2.nickname}
+                            av2={x.player2.avatar}
+                            score2={x.player2.score}
+                            />
+                        )
+                    })
+                }
             </div>
         </Container>
     );
 }
 
-function StatsTemplate(props: StatsProps) {
+function StatsTemplate(props) {
     const winsPercent = (props.total === 0 ? 0 : props.wins * 100 / props.total);
-    let lossesPercent = (props.total === 0 ? 0 : props.losses * 100 / props.total);
-    console.log('stat', props);
+    const lossesPercent = (props.total === 0 ? 0 : props.losses * 100 / props.total);
+
     return (
         <div className='text-white flex flex-col gap-5 min-[1375]:w-3/5 min-[540px]:w-2/5 mb-2'>
             <h3 className='font-semibold'>{props.for}</h3>
@@ -70,11 +113,11 @@ function StatsTemplate(props: StatsProps) {
                 <span>{props.total}</span>
             </div>
             <div className='flex flex-col gap-2'>
-                <span>Wins</span>
+                <span>{props.type1}</span>
                 <progress className="w-auto h-2 [&::-webkit-progress-bar]:rounded-lg [&::-webkit-progress-value]:rounded-lg   [&::-webkit-progress-bar]:bg-darken-300 [&::-webkit-progress-value]:bg-blueStrong [&::-moz-progress-bar]:bg-blueStrong" value={winsPercent} max='100'></progress>
             </div>
             <div className='flex flex-col gap-2'>
-                <span>Losses</span>
+                <span>{props.type2}</span>
                 <progress className="w-auto h-2 [&::-webkit-progress-bar]:rounded-lg [&::-webkit-progress-value]:rounded-lg   [&::-webkit-progress-bar]:bg-darken-300 [&::-webkit-progress-value]:bg-blueStrong [&::-moz-progress-bar]:bg-blueStrong" value={lossesPercent} max='100'></progress>
             </div>
         </div>
@@ -109,8 +152,8 @@ export function GameStats() {
         <Container>
             <h2 className='text-white'>Statistics</h2>
             <div className='flex justify-evenly flex-col min-[540px]:flex-row gap-4'>
-                <StatsTemplate for='Game' total={data.total} wins={data.wins} losses={data.loss}/>
-                <StatsTemplate for='Point' total={data.totatP} wins={data.scoreW} losses={data.scoreL}/>
+                <StatsTemplate type1="Wins" type2="Losses" for='Game' total={data.total} wins={data.wins} losses={data.loss}/>
+                <StatsTemplate type1="Goals For" type2="Goals Against " for='Point' total={data.totatP} wins={data.scoreW} losses={data.scoreL}/>
             </div>
         </Container>
     );
