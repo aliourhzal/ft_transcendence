d6bea02068f992d67334b1b544bc256d08f308c6
etahae <tahanamir@hotmail.com>
2023-08-24 02:33:40 +0100

    working on search updating convs

--- frontend/app/[nickname]/chat/components/ConvList.tsx
+++ frontend/app/[nickname]/chat/components/ConvList.tsx
@@ -1,70 +1,69 @@
 import { useContext, useEffect, useState } from "react"
 import { Context, conversation, getUsersInfo, gimmeRandom } from "../page"
 import ConvBox from "./ConvBox"
-import { userInfo } from "os"
 import Search from "./search"
 
 const ConvList = () => {
-    const {socket, set_room_created, rooms, userData, convs, setConvs, _notification, setChatBoxMessages } = useContext(Context)
-    const [updateList, setUpdateList] = useState(false)
-
-    const fillUserList = (listOfRoomsOfUser) => {
-      setConvs([])
-      console.log(listOfRoomsOfUser)
-      listOfRoomsOfUser.messages.map( (room: any) => {
-        rooms.unshift({
-          name: room.room.room.room_name,
-          lastmsg:'welcome to group chat',
-          msgs: room.msg,
-          id: room.room.room.id,
-          users: getUsersInfo(room.usersInRoom),
-          type: room.room.room.roomType
-        })
+  const {socket, set_room_created, rooms, userData, convs, setConvs, _notification, setChatBoxMessages } = useContext(Context)
+  const [updateList, setUpdateList] = useState(false)
+  
+  const fillUserList = (listOfRoomsOfUser) => {
+    setConvs([])
+    console.log(listOfRoomsOfUser)
+    listOfRoomsOfUser.messages.map( (room: any) => {
+      rooms.unshift({
+        name: room.room.room.room_name,
+        lastmsg:'welcome to group chat',
+        msgs: room.msg,
+        id: room.room.room.id,
+        users: getUsersInfo(room.usersInRoom),
+        type: room.room.room.roomType
       })
-      setConvs(rooms)
-    }
-
-    useEffect( () => {
-      socket.on('list-rooms',fillUserList)
-    }, [])
-
-    const AddUserToRoom = (res) => {
-      var _newUser;
-      const newusers = []
-      res.newUserAdded.users.map(_new => {
-        newusers.push(res.userInfos.find(o => o.userId === _new.userId))
-      })
-      if (newusers.length) {
-        newusers.map((newuser) => {
-          if (newuser.userType != 'OWNER') {
-            if (newuser.user.nickname === userData.nickname || !rooms.find(o => o.name === res.roomId.room_name)) {
-              console.log("YESSS")
-              _newUser = newuser.user.nickname
-              if (!rooms.find(o => o.id === res.roomId.id)) {
-                rooms.unshift({
-                  msgs: [],
-                  id: res.roomId.id,
-                  name: res.roomId.room_name,
-                  type: res.roomId.roomType,
-                  lastmsg:'welcome to group chat',
-                  users: getUsersInfo(res.userInfos),
-                })
-              }
-            } else {
-              rooms.find(o => o.name === res.roomId.room_name).users.push(
-                {
-                  id: newuser.user.id,
-                  nickName: newuser.user.nickname,
-                  firstName: newuser.user.firstName,
-                  lastName: newuser.user.lastName,
-                  photo: newuser.user.profilePic,
-                  type: newuser.userType,
-                  isMuted: newuser.isMuted,
-                }
-                )
-              }
+    })
+    setConvs(rooms)
+  }
+  
+  useEffect( () => {
+    socket.on('list-rooms',fillUserList)
+  }, [])
+  
+  const AddUserToRoom = (res) => {
+    var _newUser;
+    const newusers = []
+    res.newUserAdded.users.map(_new => {
+      newusers.push(res.userInfos.find(o => o.userId === _new.userId))
+    })
+    if (newusers.length) {
+      newusers.map((newuser) => {
+        if (newuser.userType != 'OWNER') {
+          if (newuser.user.nickname === userData.nickname || !rooms.find(o => o.name === res.roomId.room_name)) {
+            console.log("YESSS")
+            _newUser = newuser.user.nickname
+            if (!rooms.find(o => o.id === res.roomId.id)) {
+              rooms.unshift({
+                msgs: [],
+                id: res.roomId.id,
+                name: res.roomId.room_name,
+                type: res.roomId.roomType,
+                lastmsg:'welcome to group chat',
+                users: getUsersInfo(res.userInfos),
+              })
             }
-          })
+          } else {
+            rooms.find(o => o.name === res.roomId.room_name).users.push(
+              {
+                id: newuser.user.id,
+                nickName: newuser.user.nickname,
+                firstName: newuser.user.firstName,
+                lastName: newuser.user.lastName,
+                photo: newuser.user.profilePic,
+                type: newuser.userType,
+                isMuted: newuser.isMuted,
+              }
+              )
+            }
+          }
+        })
         setConvs([...rooms])
         console.log(rooms)
         set_room_created(old => !old)
@@ -82,31 +81,41 @@ const ConvList = () => {
     }
     
     // const removeConv = (res) => {
-    //   var kickedUser = rooms.find(o => o.name === res.roomId.room_name).users.find(o => o.id === res.kickedUser.userId)
-    //   if (kickedUser.nickname === userData.nickname) {
-    //     // var currentRoomUsers = rooms.find(o => o.name === res.roomId.room_name).users
-    //     setConvs((_convs:conversation[]) => {
-    //       var convToRemove = _convs.find(o => o.name === res.roomId.room_name)
-    //       _convs.splice(convs.indexOf(convToRemove), 1)
-    //       console.log(_convs)
-    //       return _convs
-    //     })
-    //   }
-
-    // }
-
+      //   var kickedUser = rooms.find(o => o.name === res.roomId.room_name).users.find(o => o.id === res.kickedUser.userId)
+      //   if (kickedUser.nickname === userData.nickname) {
+        //     // var currentRoomUsers = rooms.find(o => o.name === res.roomId.room_name).users
+        //     setConvs((_convs:conversation[]) => {
+          //       var convToRemove = _convs.find(o => o.name === res.roomId.room_name)
+          //       _convs.splice(convs.indexOf(convToRemove), 1)
+          //       console.log(_convs)
+          //       return _convs
+          //     })
+          //   }
+          
+          // }
+            
     useEffect(() => {
       socket.on('users-join', AddUserToRoom)
     }, [])
+      
+    const [tempSearchConv, setTempSearchConv] = useState<conversation[]>([...convs])
+
+    const convsFilter = (needle = '') => {
+      if (needle.trim() === '')
+        setTempSearchConv(convs)
+      else
+        setTempSearchConv(convs.filter((user:conversation) => (user.name.startsWith(needle))))
+    }
+    console.log(tempSearchConv)
 
     return (
-      <>
-        <Search users={convs} />
-        <div className='group left-[10%] flex-col bg-transparent w-full h-[80%] mt-8 overflow-hidden overflow-y-scroll'>
-            {convs.map ((item:conversation) =>  (<ConvBox key={gimmeRandom()} data={item} />))}
-        </div>
-      </>
-    )
-  }
+    <>
+      <Search _Filter={convsFilter} />
+      <div className='group left-[10%] flex-col bg-transparent w-full h-[80%] mt-8 overflow-hidden overflow-y-scroll'>
+          {tempSearchConv.map ((item:conversation) =>  (<ConvBox key={gimmeRandom()} data={item} />))}
+      </div>
+    </>
+  )
+}
 
   export default ConvList
--- frontend/app/[nickname]/chat/components/search.tsx
+++ frontend/app/[nickname]/chat/components/search.tsx
@@ -1,51 +1,27 @@
 import React from 'react'
-import { conversation, gimmeRandom } from '../page'
+import { conversation } from '../page'
 import { useState } from 'react'
-import Image from 'next/image'
-import { Avatar } from '@nextui-org/react'
 
-const Search = (props:any) => {
-    const [name, setName] = useState('');
-    const [foundUsers, setFoundUsers] = useState(props.users);
-    const [show, setShow] = useState(false);
+interface SearchProps {
+    _Filter: any,
+}
 
-    const filter = (e:any) => {
-        setShow(true)
-        const keyword = e.target.value;
-        if (keyword !== '') {
-            const results = props.users.filter((user:conversation) => (user.name.startsWith(keyword)))
-            setFoundUsers(results);
-        } else { if (e.target.value != '' ) setFoundUsers(props.users); else {setFoundUsers([]); setShow(false)}
-        }
-        setName(keyword)
+const Search:React.FC<SearchProps> = ( {_Filter} ) => {
+
+    const filter = (e) => {
+        _Filter(e.target.value)
     }
+
     return (
-        <>
-            <div className=' flex items-center justify-center w-[100%]'>
-                <img alt='search' src='/images/loupe.svg' width={20} height={20}/>
-                <input
-                    type='search'
-                    placeholder="Search"
-                    value={name}
-                    className="text-white pl-10 pb-5 pt-4 w-[70%] border-b border-blue-gray-200 bg-transparent  text-sm text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-blue-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                    onChange={filter} onBlur={() => {setShow(false)}}
-                />
-            { show ?
-                <div className='bg-transparent absolute w-[70%] z-10 mx-[15%] border-white h-70 overflow-scroll '>
-                    {foundUsers && foundUsers.length > 0 ? (
-                        foundUsers.map((user:conversation) => (
-                            <div className='flex justify-start h-14 bg-white text-black border-4 border-gray-800' key={gimmeRandom()}>
-                                <Avatar zoomed text={user.name} bordered color={"gradient"} className='my-2 mx-5 w-auto h-auto' src={user.photo} alt={user.name} />
-                                <span className='my-4' >{user.name}</span>
-                            </div>
-                        ))
-                        ) : (
-                        <h1 className='text-white'>No results found!</h1>
-                    )}
-                </div>
-            : ''}
-            </div>
-        </>
+        <div className=' flex items-center justify-center w-[100%]'>
+            <img alt='search' src='/images/loupe.svg' width={20} height={20}/>
+            <input
+                type='search'
+                placeholder="Search"
+                className="text-white pl-10 pb-5 pt-4 w-[70%] border-b border-blue-gray-200 bg-transparent  text-sm text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-blue-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                onChange={filter} onBlur={(e) => {e.target.value = ''}}
+            />
+        </div>
     )
 }
 
