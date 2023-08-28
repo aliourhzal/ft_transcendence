1b19aecf4dee0a6b403cb42fd8692269dd8dc769
aliourhzal <aourhzal@student.1337.ma>
2023-08-28 15:13:46 +0100

    delete old migrations and add a fresh one

--- backend/src/prisma/migrations/20230727235906_roomtype/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `roomStauts` on the `Room` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Room" DROP COLUMN "roomStauts",
-ADD COLUMN     "roomType" "RoomType" NOT NULL DEFAULT 'PUBLIC';
