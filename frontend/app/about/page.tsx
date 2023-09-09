c6142b1a983a934ec5807cdcf7f61f5cc248a91d
Asalek <ayoub.salek8599@gmail.com>
2023-09-09 21:25:05 +0100

    fix: contact page now responsive

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -12,17 +12,23 @@ export default function About()
         deleteSpeed: 60
     });
     return (
-        <div style={{backgroundImage: "url('/images/pongTable.jpeg')"}} className="bg-slate-900 bg-center bg-cover h-full w-full flex flex-col items-center justify-center overflow-y-auto">
-            <Navbar />
-                <h1 className="text-whiteSmoke text-5xl absolute max-sm:top-56 sm:top-56 z-10">
+        <div style={{backgroundImage: "url('/images/pongTable.jpeg')"}} className="bg-slate-900 relative bg-center bg-cover h-full w-full gap-10 flex flex-col items-center justify-center overflow-y-auto">
+            <div className="w-full top-0 left-0 mb-14 lg:h-5">
+                <Navbar />
+            </div>
+
+            <div className="container w-full flex justify-center">
+                <h1 className="text-whiteSmoke text-5xl">
                     I'm {' '}
                     <span className="text-blueStrong font-medium">
                         {text}
                     </span>
                     <Cursor/>
                 </h1>
-            <div className="w-full flex flex-col items-center justify-center max-sm:mt-72 sm:mt-72 gap-10 overflow-y-auto">
-                <div className="h-[65%] items-center justify-items-center content-center gap-5 grid min-sm:grid-cols-2 2xl:grid-cols-4 md:grid-cols-2">
+            </div>
+
+            <div className="container w-full h-3/4 flex static p-8 items-start justify-center gap-10 overflow-y-auto">
+                <div className="w-full h-full justify-items-center content-start gap-5 grid 2xl:grid-cols-4 md:grid-cols-2">
                     <Card />
                     <Card />
                     <Card />
