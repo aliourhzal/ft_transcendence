41acc03867433851677b5befd628f2bf87243498
Asalek <ayoub.salek8599@gmail.com>
2023-09-09 23:57:22 +0100

    update: tailwind font with Cairo and Shantell fonts

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -24,8 +24,12 @@ module.exports = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily:{
+        Shantell: ['Shantell Sans'],
+        Cairo: ['Cairo']
+      }
     },
   },
   plugins: [
