73cf5abf1e6db250000afdd6328bc3c613e6f90b
Asalek <ayoub.salek8599@gmail.com>
2023-09-10 21:33:08 +0100

    fix: page icons keep blinking according to the wrong height given

--- frontend/components/UI/AuthButton.tsx
+++ frontend/components/UI/AuthButton.tsx
@@ -14,7 +14,7 @@ export default function AuthButton(props: AuthButtonType) {
         <Link href={props.link} className='ping-slow h-14 w-14 flex justify-center items-center flex-col sm:flex-row bg-white rounded-full relative'>
             <img className='h-8 w-8' src={props.src} alt={props.alt} />
             <div className='h-full w-full rounded-full absolute bg-darken-100/40 backdrop-blur-[1px] flex justify-center items-center opacity-0'>
-                <span className='text-white opacity-0 font-medium'>{`login with ${props.alt}`}</span>
+                <span className='text-white opacity-0 font-medium h-6 text-ellipsis overflow-hidden'>{`login with ${props.alt}`}</span>
             </div>
         </Link>
     );
