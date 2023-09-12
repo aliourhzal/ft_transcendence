163d2d64c3e4c4f1e2a8e8724a925797779a2ec5
Asalek <ayoub.salek8599@gmail.com>
2023-09-12 19:52:20 +0100

    add: loading game page file

---
+++ frontend/app/[nickname]/game/loading.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './style.css'
+
+const loading = () => {
+  return (
+    <div className='bg-darken-200 flex items-center justify-center w-full h-full overflow-auto'>
+      <div className="loader">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+      </div>
+    </div>
+  )
+}
+
+export default loading
