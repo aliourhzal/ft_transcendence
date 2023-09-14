67a934bc58fdb09a0d1f3351eeb2f89fe1fb8b04
Asalek <ayoub.salek8599@gmail.com>
2023-09-14 17:15:23 +0100

    update: game socket with a useEffect to connect socket when ever it disconnect

--- frontend/app/contexts/gameWebSocket.tsx
+++ frontend/app/contexts/gameWebSocket.tsx
@@ -1,14 +1,37 @@
 'use client'
 import { io, Socket } from "socket.io-client";
-import { createContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { getCookie } from "../[nickname]/layout";
 
 export const socket = io(`http://${process.env.NEXT_PUBLIC_BACK}:3003`, {
     auth: {
         token: getCookie('access_token')
-    }
+    },
+    // autoConnect: true
 });
 export const WebsocketContext = createContext<Socket>(socket);
 
+interface Props {
+    children : React.ReactNode
+}
+
+export function WebSocketProvider({ children } : Props)
+{
+    const [Gsocket, setGameSocket] = useState<Socket>(socket);
+
+    useEffect(() => {
+        socket.connect();
+        setGameSocket(socket);
+        return () => {
+            socket.disconnect();
+        };
+    }
+    ,[socket.connected]);
+
+    return (
+      <WebsocketContext.Provider value={Gsocket}>{children}</WebsocketContext.Provider>
+    );
+}
+
 //or you can just use <WebsocketContext.Provider> </>
-export const webSocketProvider = WebsocketContext.Provider;
+// export const WebSocketProvider = WebsocketContext.Provider;
