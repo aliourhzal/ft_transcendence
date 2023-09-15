006525d8a8078cf94a664e9c6fdb5d49114dbb61
Asalek <ayoub.salek8599@gmail.com>
2023-09-15 22:30:54 +0100

    add: loading for contact component now available

---
+++ frontend/app/contact/loading.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './style.css'
+
+export default function loading()
+{
+    return (
+        <div className=' bg-darken-200 w-full h-full overflow-auto flex justify-center items-center'>
+            <div className="wrapper">
+                <div className="circle"></div>
+                <div className="circle"></div>
+                <div className="circle"></div>
+                <div className="shadow"></div>
+                <div className="shadow"></div>
+                <div className="shadow"></div>
+            </div>
+        </div>
+    );
+}
