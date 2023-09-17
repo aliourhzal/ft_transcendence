e4711e5e4516c564bd99f472efeb8374158450df
Asalek <ayoub.salek8599@gmail.com>
2023-09-17 19:30:56 +0100

    fix: remove all exported member functions and locate them in a new file

--- frontend/app/[nickname]/chat/page.tsx
+++ frontend/app/[nickname]/chat/page.tsx
@@ -18,6 +18,7 @@ import UserInfo from "./components/UserInfo";
 import MyAlert from "./components/MyAlert";
 import axios from "axios";
 import { setRef } from "@mui/material";
+import Context from "./components/Context";
 
 export interface conversation {
 	readonly name: string,
@@ -28,12 +29,7 @@ export interface conversation {
 	readonly pending?: boolean
 }
 
-export const Context = createContext<any>(undefined)
 
-export const gimmeRandom = () => {
-	const date = new Date();
-	return Math.random()
-};
 
 export interface Room {
 	msgs: {userId:string, msg:string}[],
@@ -55,63 +51,6 @@ export interface Room {
 	pending: boolean
 }
 
-export const getUsersInfo = (users) => {
-	let _users: {
-		  id: string,
-		  nickName: string,
-		  firstName: string,
-		  lastName: string,
-		  photo?: string,
-		  cover?: string,
-		  type: "OWNER"| "ADMIN" | "USER",
-		  isMuted: string
-	  }[] = []
-	users.map( (user) => {
-		_users.push(
-			{
-			id: user.user.id,
-			nickName: user.user.nickname,
-			firstName: user.user.firstName,
-			lastName: user.user.lastName,
-			photo: user.user.profilePic,
-			cover: user.user.coverPic,
-			type: user.userType,
-			isMuted: user.isMuted,
-			}
-		)
-	  } 
-	)
-	return (_users)
-}
-
-export const setDmUsers = (users) => {
-	let _users: {
-		  id: string,
-		  nickName: string,
-		  firstName: string,
-		  lastName: string,
-		  photo?: string,
-		  cover?: string,
-		  type: "OWNER"| "ADMIN" | "USER",
-		  isMuted: string
-	  }[] = []
-	users.map( (user) => {
-		_users.push(
-			{
-			id: user.id,
-			nickName: user.nickname,
-			firstName: user.firstName,
-			lastName: user.lastName,
-			photo: user.profilePic,
-			cover: user.coverPic,
-			type: 'USER',
-			isMuted: 'UNMUTED',
-			}
-		)
-	  } 
-	)
-	return (_users)
-}
 
 const _cookie = getCookie('access_token')
 
@@ -276,7 +215,6 @@ export default function Chat() {
 
 	if (!getCookie('access_token')) {
 		document.cookie = "access_token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
-		const router = useRouter()
 		router.push('/')
 	}
 
