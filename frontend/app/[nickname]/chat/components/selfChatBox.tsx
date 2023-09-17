37fb78d63698193a3ac366656c17e8ff9bc58d67
Asalek <ayoub.salek8599@gmail.com>
2023-09-17 17:42:22 +0100

    fix: chat missing alt option

--- frontend/app/[nickname]/chat/components/selfChatBox.tsx
+++ frontend/app/[nickname]/chat/components/selfChatBox.tsx
@@ -15,7 +15,7 @@ const SelfChatBox:React.FC<SelfChatBoxProps> = (msg) => {
             :
                 msg.msg
             }</div>
-            <img className="rounded-full w-30 h-30 border-2 border-slate-300 w-10 h-10" width={10} height={10} src={msg.user ? msg.user.photo : '/images/unknownUser.png'}/>
+            <img className="rounded-full w-30 h-30 border-2 border-slate-300 w-10 h-10" width={10} height={10} src={msg.user ? msg.user.photo : '/images/unknownUser.png'} alt=""/>
             {/* <Avatar color={"primary"} src={msg.user ? msg.user.photo : '/images/unknownUser.png'}/> */}
         </div>
     )
