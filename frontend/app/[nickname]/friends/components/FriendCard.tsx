d2c1dcda101b788761b13b94679487a3b6147aea
Asalek <ayoub.salek8599@gmail.com>
2023-09-21 18:58:48 +0100

    fix: return the card friend bottom bar

--- frontend/app/[nickname]/friends/components/FriendCard.tsx
+++ frontend/app/[nickname]/friends/components/FriendCard.tsx
@@ -1,8 +1,12 @@
 import Container from "@/components/UI/ProfileBoxs";
 import { UniversalData, userDataContext } from "../../../contexts/UniversalData";
 import Link from "next/link"
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { InvitationSocketContext } from "@/app/contexts/InvitationWebSocket";
+import { HiExternalLink } from "react-icons/hi"
+import { RiPingPongFill } from "react-icons/ri"
+import { RiDeleteBin6Fill } from "react-icons/ri"
+import { BiSolidMessageSquareDots } from "react-icons/bi"
 
 import Game from "../../game/Game";
 
@@ -12,44 +16,65 @@ export default function FriendCard({user, setPlay}: {
 	setPlay: Function
 }) {
 	const [lvl, progess] = user.level.toString().split('.');
-	const loggedUser = useContext(userDataContext);
 	const socket = useContext(InvitationSocketContext);
+	const loggedUser = useContext(userDataContext);
 	
 	function removeFriend() {
 		socket.emit('delete-friend', user.id);
 	}
 	
 	return (
-		<Container className="p-0 overflow-hidden flex flex-col items-center relative">
-			<div style={{backgroundImage: `url(${user.coverPic})`}} className="bg-cover bg-center bg-no-repeat w-full h-[150px] rounded-lg"></div>
-			<img src={user.profilePic} alt="avatar" className="h-[130px] w-[130px] rounded-full mt-[-90px]" />
-			<div className="flex flex-col gap-3 items-center w-full">
-				<span className="text-white font-medium text-lg">{user.nickname}</span>
-				<div className="flex justify-evenly w-full">
-					<div className="flex flex-col items-center">
-						<span className="text-blueStrong font-medium">wallet</span>
-						<span className="text-white">{user.wallet}</span>
+		<>
+			<div className="flex flex-col gap-3">
+				<Container className="p-0 overflow-hidden flex flex-col items-center relative">
+					<div style={{backgroundImage: `url(${user.coverPic})`}} className="bg-cover bg-center bg-no-repeat w-full h-[150px] rounded-lg"></div>
+					<img src={user.profilePic} alt="avatar" className="h-[130px] w-[130px] rounded-full mt-[-90px]" />
+					<div className="flex flex-col gap-3 items-center w-full">
+						<span className="text-white font-medium text-lg">{user.nickname}</span>
+						<div className="flex justify-evenly w-full">
+							<div className="flex flex-col items-center">
+								<span className="text-blueStrong font-medium">wallet</span>
+								<span className="text-white">{user.wallet}</span>
+							</div>
+							<div className="flex flex-col items-center">
+								<span className="text-blueStrong font-medium">level</span>
+								<span className="text-white">{lvl}</span>
+							</div>
+						</div>
+						<div className="w-full h-[30px] bg-darken-300 rounded-full">
+							<div style={{width: `${progess}%`}} className={` bg-blueStrong h-full rounded-full flex items-center justify-end p-1`}>
+								<span className="text-[12px] font-medium text-white">{`${progess}%`}</span>
+							</div>
+						</div>
 					</div>
-					<div className="flex flex-col items-center">
-						<span className="text-blueStrong font-medium">level</span>
-						<span className="text-white">{lvl}</span>
-					</div>
-				</div>
-				<div className="w-full h-[30px] bg-darken-300 rounded-full">
-					<div style={{width: `${progess}%`}} className={` bg-blueStrong h-full rounded-full flex items-center justify-end p-1`}>
-						<span className="text-[12px] font-medium text-white">{`${progess}%`}</span>
-					</div>
-				</div>
+					{/* <div className="flex justify-between w-full">
+						<Link href={`/${user.nickname}`} className="p-2 bg-green-500 text-white font-medium rounded-xl">See Profile</Link>
+						<Link href={`/${loggedUser.nickname}/game?id=${user.id}`} className="p-2 bg-blue-500 text-white font-medium rounded-xl">Play</Link>
+						<button onClick={removeFriend} className="p-2 bg-red-500 text-white font-medium rounded-xl">Remove Friend</button>
+					</div> */}
+					{
+						user.status === 'offline' && <div className="absolute top-0 left-0 h-full w-full z-10 bg-black opacity-50"></div>
+					}
+				</Container>
+				<Container className="flex flex-row items-center justify-evenly relative overflow-hidden">
+						<Link href={`/${user.nickname}`}>
+							<HiExternalLink size="1.75rem" color="white" cursor="pointer"/>
+						</Link>
+						<Link href={`/${loggedUser.nickname}/game?id=${user.id}`}>
+							<RiPingPongFill size="1.5rem" color="white" cursor="pointer"/>
+						</Link>
+						<Link href={`/${loggedUser.nickname}/chat?id=${user.id}`}>
+							<BiSolidMessageSquareDots size="1.5rem" color="white" cursor="pointer"/>
+						</Link>
+						<button onClick={removeFriend}>
+							<RiDeleteBin6Fill size="1.5rem" color="white" cursor="pointer"/>
+						</button>
+						{
+							user.status === 'offline' && <div className="absolute top-0 left-0 h-full w-full z-10 bg-black opacity-50"></div>
+						}
+				</Container>
 			</div>
-			<div className="flex justify-between w-full">
-				<Link href={`/${user.nickname}`} className="p-2 bg-green-500 text-white font-medium rounded-xl">See Profile</Link>
-				<Link href={`/${loggedUser.nickname}/chat?id=${user.id}`} className="p-2 bg-blue-500 text-white font-medium rounded-xl">DM</Link>
-				<button onClick={removeFriend} className="p-2 bg-red-500 text-white font-medium rounded-xl">Remove Friend</button>
-			</div>
-			{
-				user.status === 'offline' && <div className="absolute top-0 left-0 h-full w-full z-10 bg-black opacity-50"></div>
-			}
-		</Container>
+		</>
 	);
 }
 
