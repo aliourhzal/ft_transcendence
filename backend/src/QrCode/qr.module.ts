009d55b1c50205cf4feaced8c5937eaa25ca70ba
Asalek <ayoub.salek8599@gmail.com>
2023-09-24 01:32:34 +0100

    update: QrCode Module with JwtPrivateOrSecretKey and AuthService provider

--- backend/src/QrCode/qr.module.ts
+++ backend/src/QrCode/qr.module.ts
@@ -5,11 +5,16 @@ import { twoFactorAuth } from './qr.services';
 import { JwtStrategy } from 'src/auth/jwt.strategy';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthModule } from 'src/auth/auth.module';
+import { AuthService } from 'src/auth/auth.service';
 
 @Module({
-    imports: [UsersModule, JwtModule],
+    imports: [UsersModule,
+        JwtModule.register({
+            secret: process.env.JWT_SECRET // used to sign/create a JWT //*
+        })
+    ],
     controllers: [QrController],
-    providers: [twoFactorAuth],//what services do we want to be initialized/used within our module
+    providers: [twoFactorAuth, AuthService],//what services do we want to be initialized/used within our module
     exports: []//what services do we want to be consumed by other modules
 })
 
